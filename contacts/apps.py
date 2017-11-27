912e8ec9a532c1da4029d386fe6a8e1dc2919429
raeeschachar <raees.chachar@arbisoft.com>
2017-11-27 23:27:49 +0500

    Changed app name to plural

--- contacts/apps.py
+++ contacts/apps.py
@@ -5,4 +5,4 @@ from django.apps import AppConfig
 
 
 class MyDirectoryConfig(AppConfig):
-    name = 'contact'
+    name = 'contacts'
--- contacts/migrations/0002_auto_20171127_1817.py
+++ contacts/migrations/0002_auto_20171127_1817.py
@@ -9,7 +9,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contact', '0001_initial'),
+        ('contacts', '0001_initial'),
     ]
 
     operations = [
