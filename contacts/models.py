56d8d83b29d397cb319bdcdc6c0a8fbde4652106
raeeschachar <raees.chachar@arbisoft.com>
2017-11-28 20:54:58 +0500

    Added address as a seperate model

--- contacts/models.py
+++ contacts/models.py
@@ -8,10 +8,21 @@ class Contact(models.Model):
             message="Phone number must be entered in the format: '+999999999'. Up to 20 digits allowed."
     )
 
-    name = models.CharField(max_length=100)
-    address = models.CharField(max_length=200)
+    name = models.CharField(max_length=50)
     email = models.EmailField(max_length=30)
     phone_number = models.CharField(validators=[phone_regex], max_length=20, blank=True)
 
     def __str__(self):
         return self.name
+
+
+class Address(models.Model):
+    address = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    address_line = models.TextField(max_length=50)
+    city = models.TextField(max_length=15)
+    state = models.TextField(max_length=15)
+    zip = models.TextField(max_length=15)
+    country = models.TextField(max_length=20)
+
+    def __str__(self):
+        return self.country
