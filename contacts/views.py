2f4eafd8395d2a8595e382bc9a8fdd2cc77b4721
raeeschachar <raees.chachar@arbisoft.com>
2017-12-01 03:54:07 +0500

    Added first user facing contacts page

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,13 @@
-from django.http import HttpResponse
+from django.views import generic
+from .models import Contact
 
 
-def index(request):
-    return HttpResponse("Sample View")
+class ContactView(generic.ListView):
+    template_name = 'contacts/contact.html'
+    context_object_name = 'my_contacts'
+
+    def get_queryset(self):
+        """
+        Return contacts by name
+        """
+        return Contact.objects.order_by('name')
