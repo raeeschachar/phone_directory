8d043279cb2412441d55a718b9ac274000c866a7
raeeschachar <raees.chachar@arbisoft.com>
2017-12-01 11:01:58 +0500

    Added user facing detail page part 1

--- contacts/urls.py
+++ contacts/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 app_name = 'contacts'
 urlpatterns = [
-    url(r'^$', views.ContactView.as_view(), name='contact')
+    url(r'^$', views.ContactView.as_view(), name='contact'),
+    # url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^([0-9]+)/$', views.detail, name='detail')
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import render
 from django.views import generic
-from .models import Contact
+from .models import Contact, Address
 
 
 class ContactView(generic.ListView):
@@ -11,3 +12,22 @@ class ContactView(generic.ListView):
         Return contacts by name
         """
         return Contact.objects.order_by('name')
+
+
+# class DetailView(generic.DetailView):
+#     model = Contact
+#     template_name = 'contacts/detail.html'
+#
+#     def get_queryset(self):
+#         """
+#         Return details of contact
+#         """
+#         return Contact.objects.get(id=Contact.id)
+
+
+def detail(request, contact_id):
+    contact_details = Contact.objects.get(id=contact_id)
+    address_details = Address.objects.get(id=contact_id)
+    return render(request, 'contacts/detail.html', {
+        'contact_details': contact_details, 'address_details': address_details
+    })
