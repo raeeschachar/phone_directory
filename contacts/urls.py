ca59a4611ea7dc879686c109b04c6ce5f6df4867
raeeschachar <raees.chachar@arbisoft.com>
2017-12-05 00:10:39 +0500

    Fixed views and htmls

--- contacts/urls.py
+++ contacts/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 app_name = 'contacts'
 urlpatterns = [
-    url(r'^$', views.ContactView.as_view(), name='contact'),
-    # url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^([0-9]+)$', views.detail, name='detail')
+    url(r'^$', views.ContactsListView.as_view(), name='contact'),
+    url(r'^(?P<pk>[0-9]+)/?$', views.ContactDetailView.as_view(), name='detail'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,33 +1,11 @@
-from django.shortcuts import render
 from django.views import generic
-from .models import Contact, Address
+from .models import Contact
 
 
-class ContactView(generic.ListView):
-    template_name = 'contacts/contact.html'
+class ContactsListView(generic.ListView):
     context_object_name = 'my_contacts'
-
-    def get_queryset(self):
-        """
-        Return contacts by name
-        """
-        return Contact.objects.order_by('name')
+    queryset = Contact.objects.order_by('name')
 
 
-# class DetailView(generic.DetailView):
-#     model = Contact
-#     template_name = 'contacts/detail.html'
-#
-#     def get_queryset(self):
-#         """
-#         Return details of contact
-#         """
-#         return Contact.objects.get(id=Contact.id)
-
-
-def detail(request, contact_id):
-    contact_details = Contact.objects.get(id=contact_id)
-    address_details = Address.objects.get(id=contact_details.id)
-    return render(request, 'contacts/detail.html', {
-        'contact_details': contact_details, 'address_details': address_details
-    })
+class ContactDetailView(generic.DetailView):
+    model = Contact
