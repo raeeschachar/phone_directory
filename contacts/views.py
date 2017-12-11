a1319e7c42f2cf12ede42cd2fbd4e66fd9d5ed97
raeeschachar <raees.chachar@arbisoft.com>
2017-12-12 01:19:46 +0500

    everything is working

--- contacts/views.py
+++ contacts/views.py
@@ -14,9 +14,10 @@ class ContactDetailView(generic.DetailView):
 class AddContactView(generic.CreateView):
     model = Contact
     fields = ['name', 'email', 'phone_number']
+    template_name = 'contacts/form.html'
 
 
 class AddContactAddressView(generic.CreateView):
     model = Address
     fields = ['contact', 'address_selection', 'address_line', 'city', 'state', 'zip_code', 'country']
-    template_name = 'contacts/contact_form.html'
+    template_name = 'contacts/form.html'
