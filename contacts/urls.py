be5bdd8122e95b3f829c5ea03d494822393a7888
raeeschachar <raees.chachar@arbisoft.com>
2017-12-29 22:31:42 +0500

    Fixed naming

--- contacts/urls.py
+++ contacts/urls.py
@@ -6,8 +6,8 @@ from . import views
 app_name = 'contacts'
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view(), name='home'),
-    url(r'^contact/?$', views.ContactsListView.as_view(), name='contact'),
-    url(r'^(?P<pk>[0-9]+)/?$', views.ContactDetailView.as_view(), name='detail'),
+    url(r'^contact/?$', views.ContactsListView.as_view(), name='contact_list'),
+    url(r'^(?P<pk>[0-9]+)/?$', views.ContactDetailView.as_view(), name='contact_detail'),
     url(r'^add_contact/?$', views.AddContactView.as_view(), name='add_contact'),
     url(r'^add_address/?$', views.AddContactAddressView.as_view(), name='add_address'),
     url(r'^update_contact/(?P<pk>[0-9]+)/?$', views.UpdateContactView.as_view(), name='update_contact'),
