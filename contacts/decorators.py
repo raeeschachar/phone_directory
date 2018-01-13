92e3915fb209fab2c11f6b0e9491632735df6079
raeeschachar <raees.chachar@arbisoft.com>
2018-01-13 23:58:16 +0500

    Custom decorator (#14)
    * Custom decorator
    
    * Trying to fix decorator
    
    * Fixed decorator
    
    * Addressed comments
    
    * Fixed as per comments
    
    * Fixed decorator name

---
+++ contacts/decorators.py
@@ -0,0 +1,19 @@
+from functools import wraps
+from django.core.exceptions import PermissionDenied
+
+from .models import Contact, Address
+
+
+def user_has_permission(view_func):
+    def _decorator(request, *args, **kwargs):
+        if kwargs.get('contact_id') and \
+                Contact.objects.filter(pk=kwargs.get('contact_id'), user=request.user).exists():
+            return view_func(request, *args, **kwargs)
+
+        elif kwargs.get('address_id') and \
+                Address.objects.filter(pk=kwargs.get('address_id'), contact__user=request.user).exists():
+            return view_func(request, *args, **kwargs)
+
+        raise PermissionDenied
+
+    return wraps(view_func)(_decorator)
